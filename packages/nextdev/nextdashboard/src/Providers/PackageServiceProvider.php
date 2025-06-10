93dac7b76047d41d2ca997c04ec5793b95fce2c6
MahmoudNagahNext <m.nagah@nextecontech.ai>
2025-06-10 21:19:49 +0300

    package folders structuer

---
+++ packages/nextdev/nextdashboard/src/Providers/PackageServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace nextdev\nextdashboard;
+
+use Illuminate\Support\ServiceProvider;
+
+class PackageServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        // Load routes
+        $this->loadRoutesFrom(__DIR__.'/../routes/api.php');
+
+        // Load migrations
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+
+        // Publish config
+        $this->publishes([
+            __DIR__.'/../config/config.php' => config_path('config.php'),
+        ]);
+
+        // Publish migrations
+        $this->publishes([
+            __DIR__.'/../database/migrations' => database_path('migrations'),
+        ], 'nextdashboard-migrations');
+    }
+
+    public function register()
+    {
+        // // Merge package config
+        // $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'laravelusermanager');
+    }
+}
